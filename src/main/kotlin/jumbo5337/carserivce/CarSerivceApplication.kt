37d8962663d0c3f83d0a77323ab3d27501ca138d
o.lobanov <o.lobanov@crpt.ru>
2022-11-19 15:13:08 +0300

    chore: init repo

---
+++ src/main/kotlin/jumbo5337/carserivce/CarSerivceApplication.kt
@@ -0,0 +1,11 @@
+package jumbo5337.carserivce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarSerivceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CarSerivceApplication>(*args)
+}
---
+++ src/test/kotlin/jumbo5337/carserivce/CarSerivceApplicationTests.kt
@@ -0,0 +1,13 @@
+package jumbo5337.carserivce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarSerivceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
